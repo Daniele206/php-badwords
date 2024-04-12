ad3477427000f633186080ab26f8672dfd3b289b
Daniele206 <danieledeieso2004@gmail.com>
2024-04-12 15:05:57 +0200

    [add] word & text da form to recive

--- recive.php
+++ recive.php
@@ -1,6 +1,7 @@
 <?php 
 
-
+$word = $_POST['word'];
+$text = $_POST['text'];
 
 ?>
 
@@ -8,10 +9,22 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>recive PHP</title>
 </head>
 <body>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col col-6 border-1 border-black">
+        <h1 class="fw-bold text-danger">IL testo sensa censura é:</h1>
+        <p><?php echo $text ?></p>
+      </div>
+      <div class="col col-6 border-1 border-black">
+      <h1 class="fw-bold text-success">IL testo con la censura é:</h1>
+        <p><?php echo $text ?></p>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
