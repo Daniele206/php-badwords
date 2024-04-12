00e04213d3349e7481c867835019be40dd23e9c9
Daniele206 <danieledeieso2004@gmail.com>
2024-04-12 15:16:11 +0200

    [add] word-number of text

--- recive.php
+++ recive.php
@@ -1,7 +1,9 @@
 <?php 
 
 $word = $_POST['word'];
-$text = $_POST['text'];
+$text_1 = $_POST['text'];
+
+$text_1_lenght = strlen($text_1);
 
 ?>
 
@@ -18,11 +20,13 @@ $text = $_POST['text'];
     <div class="row">
       <div class="col col-6 border-1 border-black">
         <h1 class="fw-bold text-danger">IL testo sensa censura é:</h1>
-        <p><?php echo $text ?></p>
+        <p><?php echo $text_1 ?></p>
+        <span class="fs-2">Il testo é formato da <strong><?php echo $text_1_lenght ?></strong> parole</span>
       </div>
       <div class="col col-6 border-1 border-black">
       <h1 class="fw-bold text-success">IL testo con la censura é:</h1>
-        <p><?php echo $text ?></p>
+        <p><?php echo $text_2 ?></p>
+        <span class="fs-2">Il testo é formato da <strong><?php echo $text_2_lenght ?></strong> parole</span>
       </div>
     </div>
   </div>
